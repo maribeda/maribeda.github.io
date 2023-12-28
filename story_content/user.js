2659398bf23346ddabacdaee4f540a0112fe2805
maribeda <imumuslak@gmail.com>
2023-12-28 11:27:16 +0700

    Upload Awal

---
+++ story_content/user.js
@@ -0,0 +1,118 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6HwbnUe6JVl":
+        Script1();
+        break;
+      case "5pOE0KdWadh":
+        Script2();
+        break;
+      case "6LJc0T3ELOn":
+        Script3();
+        break;
+      case "6mdifGoTm1y":
+        Script4();
+        break;
+      case "6pmrUHeXG3e":
+        Script5();
+        break;
+      case "5oo1QGcVQKm":
+        Script6();
+        break;
+      case "6PiATELxxu0":
+        Script7();
+        break;
+      case "68MWRhLVLxW":
+        Script8();
+        break;
+      case "6SeHPXeXfGx":
+        Script9();
+        break;
+      case "6p9fM2BMLQC":
+        Script10();
+        break;
+      case "6MRbuMmcSrK":
+        Script11();
+        break;
+      case "6FxxfQPia0X":
+        Script12();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="musik.mp3";
+audio.load();
+audio.play();
+audio.volume = 0.4;
+}
+
+function Script2()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.0;
+}
+
+function Script3()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.1;
+}
+
+function Script4()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.2;
+}
+
+function Script5()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.3;
+}
+
+function Script6()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.4;
+}
+
+function Script7()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.5;
+}
+
+function Script8()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.6;
+}
+
+function Script9()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.7;
+}
+
+function Script10()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.8;
+}
+
+function Script11()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.9;
+}
+
+function Script12()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 1.0;
+}
+
